17d0e4f676b7f281ecc19ae8936bd2ed42ef454f
Filip Bezjak <Filip.bezjak@gmail.com>
2020-11-02 13:31:07 +0100

    naložl html

---
+++ neka-mapa.py/neki-folder.py
@@ -0,0 +1,186 @@
+import csv
+import os
+import requests
+import re
+###############################################################################
+# Najprej definirajmo nekaj pomožnih orodij za pridobivanje podatkov s spleta.
+###############################################################################
+
+# definiratje URL glavne strani bolhe za oglase z mačkami
+cats_frontpage_url = 'http://www.bolha.com/zivali/male-zivali/macke/'
+# mapa, v katero bomo shranili podatke
+cat_directory = 'cats'
+# ime datoteke v katero bomo shranili glavno stran
+frontpage_filename = 'frontpage.html'
+# ime CSV datoteke v katero bomo shranili podatke
+csv_filename = 'csv-db.csv'
+
+
+def download_url_to_string(url):
+    """Funkcija kot argument sprejme niz in puskuša vrniti vsebino te spletne
+    strani kot niz. V primeru, da med izvajanje pride do napake vrne None.
+    """
+    try:
+        r = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        print('Napaka pri povezovanju')
+        return None
+    if r.status_code == requests.codes.ok:
+        return r.text
+
+
+def save_string_to_file(text, directory, filename):
+    """Funkcija zapiše vrednost parametra "text" v novo ustvarjeno datoteko
+    locirano v "directory"/"filename", ali povozi obstoječo. V primeru, da je
+    niz "directory" prazen datoteko ustvari v trenutni mapi.
+    """
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
+    with open(path, 'w', encoding='utf-8') as file_out:
+        file_out.write(text)
+    return None
+
+
+# Definirajte funkcijo, ki prenese glavno stran in jo shrani v datoteko.
+
+
+def save_frontpage(page, directory, filename):
+    """Funkcija shrani vsebino spletne strani na naslovu "page" v datoteko
+    "directory"/"filename"."""
+    text = download_url_to_string(page)
+    if text:
+        save_string_to_file(text, directory, filename)
+    else:
+        raise ValueError(f'No content found in {page}')
+
+
+
+###############################################################################
+# Po pridobitvi podatkov jih želimo obdelati.
+###############################################################################
+
+
+def read_file_to_string(directory, filename):
+    """Funkcija vrne celotno vsebino datoteke "directory"/"filename" kot niz"""
+    with open(os.path.join(directory, filename)) as f:
+        return f.read()
+
+
+# Definirajte funkcijo, ki sprejme niz, ki predstavlja vsebino spletne strani,
+# in ga razdeli na dele, kjer vsak del predstavlja en oglas. To storite s
+# pomočjo regularnih izrazov, ki označujejo začetek in konec posameznega
+# oglasa. Funkcija naj vrne seznam nizov.
+
+
+def page_to_ads(page_content):
+    """Funkcija poišče posamezne ogllase, ki se nahajajo v spletni strani in
+    vrne njih seznam"""
+    pattern = r'<li class="EntityList-item EntityList-item--Regular.*?</article>.*?</li>'
+    return re.findall(pattern, page_content, flags=re.DOTALL)
+
+
+
+# Definirajte funkcijo, ki sprejme niz, ki predstavlja oglas, in izlušči
+# podatke o imenu, ceni in opisu v oglasu.
+
+
+def get_dict_from_ad_block(block):
+    """Funkcija iz niza za posamezen oglasni blok izlušči podatke o imenu, ceni
+    in opisu ter vrne slovar, ki vsebuje ustrezne podatke
+    """
+    #ime cena opis
+    r_naslov = r'<h3.*>(?P<naslov>.*?)</a></h3>'
+    r_cena = r'<strong class=".*?">.*?(?P<cena>[0-9]+)'
+    r_opis_url = r'href="/(?P<url>.*?)"'
+
+    naslov = re.search(r_naslov, block).group('naslov')
+    cena = re.search(r_cena, block, flags=re.DOTALL).group('cena')
+    opis_url = re.search(r_opis_url, block).group('url')
+
+    new_url = cats_frontpage_url + opis_url
+    #oglas_stran = download_url_to_string(new_url)
+
+
+    return({'naslov': naslov, 'cena': cena})
+
+
+# Definirajte funkcijo, ki sprejme ime in lokacijo datoteke, ki vsebuje
+# besedilo spletne strani, in vrne seznam slovarjev, ki vsebujejo podatke o
+# vseh oglasih strani.
+
+
+def ads_from_file(filename, directory):
+    """Funkcija prebere podatke v datoteki "directory"/"filename" in jih
+    pretvori (razčleni) v pripadajoč seznam slovarjev za vsak oglas posebej."""
+    data = read_file_to_string(directory, filename)
+    blocks = page_to_ads(data)
+    return [get_dict_from_ad_block(ad_block) for ad_block in blocks]
+
+
+###############################################################################
+# Obdelane podatke želimo sedaj shraniti.
+###############################################################################
+
+
+def write_csv(fieldnames, rows, directory, filename):
+    """
+    Funkcija v csv datoteko podano s parametroma "directory"/"filename" zapiše
+    vrednosti v parametru "rows" pripadajoče ključem podanim v "fieldnames"
+    """
+    os.makedirs(directory, exist_ok=True)
+    path = os.path.join(directory, filename)
+    with open(path, 'w') as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+        writer.writeheader()
+        for row in rows:
+            writer.writerow(row)
+    return
+
+
+# Definirajte funkcijo, ki sprejme neprazen seznam slovarjev, ki predstavljajo
+# podatke iz oglasa mačke, in zapiše vse podatke v csv datoteko. Imena za
+# stolpce [fieldnames] pridobite iz slovarjev.
+
+
+def write_cat_ads_to_csv(ads, directory, filename):
+    """Funkcija vse podatke iz parametra "ads" zapiše v csv datoteko podano s
+    parametroma "directory"/"filename". Funkcija predpostavi, da sa ključi vseh
+    sloverjev parametra ads enaki in je seznam ads neprazen.
+    """
+    # Stavek assert preveri da zahteva velja
+    # Če drži se program normalno izvaja, drugače pa sproži napako
+    # Prednost je v tem, da ga lahko pod določenimi pogoji izklopimo v
+    # produkcijskem okolju
+    assert ads and (all(j.keys() == ads[0].keys() for j in ads))
+    fieldnames = list(ads[0].keys())
+    write_csv(fieldnames, ads, directory, filename)
+
+
+# Celoten program poženemo v glavni funkciji
+
+def main(redownload=True, reparse=True):
+    """Funkcija izvede celoten del pridobivanja podatkov:
+    1. Oglase prenese iz bolhe
+    2. Lokalno html datoteko pretvori v lepšo predstavitev podatkov
+    3. Podatke shrani v csv datoteko
+    """
+    # Najprej v lokalno datoteko shranimo glavno stran
+    try:
+        save_frontpage(cats_frontpage_url, cat_directory, frontpage_filename)
+    except ValueError:
+        print('Stran ni bila shranjena!')
+
+    # Podatke prebermo v lepšo obliko (seznam slovarjev)
+    ads = ads_from_file(frontpage_filename, cat_directory)
+
+    # Podatke shranimo v csv datoteko
+    write_cat_ads_to_csv(ads, cat_directory, csv_filename)
+
+
+    # Dodatno: S pomočjo parametrov funkcije main omogoči nadzor, ali se
+    # celotna spletna stran ob vsakem zagon prense (četudi že obstaja)
+    # in enako za pretvorbo
+
+
+if __name__ == '__main__':
+    main()
---
+++ neka-mapa.py/poberi.py
@@ -0,0 +1,26 @@
+import orodja
+import re
+import requests
+
+STEVILO_STRANI =20
+
+vzorec = (
+    r'class="GO-Results-Naziv bg-dark px-3 py-2 font-weight-bold text-truncate text-white text-decoration-none">\s*'
+    r'<span>(?P<imeavta>.*?)</span>'
+)
+
+
+najdeni_filmi = 0
+
+for stran in range(STEVILO_STRANI):
+    stran += 1
+    url = f'https://www.avto.net/Ads/results.asp?znamka=&model=&modelID=&tip=&znamka2=&model2=&tip2=&znamka3=&model3=&tip3=&cenaMin=0&cenaMax=999999&letnikMin=0&letnikMax=2090&bencin=0&starost2=999&oblika=11&ccmMin=0&ccmMax=99999&mocMin=&mocMax=&kmMin=0&kmMax=9999999&kwMin=0&kwMax=999&motortakt=0&motorvalji=0&lokacija=0&sirina=0&dolzina=&dolzinaMIN=0&dolzinaMAX=100&nosilnostMIN=0&nosilnostMAX=999999&lezisc=&presek=0&premer=0&col=0&vijakov=0&EToznaka=0&vozilo=&airbag=&barva=&barvaint=&EQ1=1000000000&EQ2=1000000000&EQ3=1000000000&EQ4=100000000&EQ5=1000000000&EQ6=1000000000&EQ7=1110100020&EQ8=1010000001&EQ9=1000000000&KAT=1010000000&PIA=&PIAzero=&PSLO=&akcija=0&paketgarancije=&broker=0&prikazkategorije=0&kategorija=0&zaloga=10&arhiv=0&presort=3&tipsort=DESC&stran={stran}'
+    datoteka = f'Avti-str={stran}.html'
+    orodja.shrani_spletno_stran(url, datoteka)
+    vsebina = orodja.vsebina_datoteke(datoteka)
+
+    for zadetek in re.finditer(vzorec, vsebina):
+        print(zadetek.groupdict())
+        najdeni_filmi += 1
+
+print(najdeni_filmi)
